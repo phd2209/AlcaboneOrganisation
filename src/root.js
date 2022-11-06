f79a37e219c4b84500085c1f65a7b0b5dfd66e1b
Toni Awwad <toni.awwad@me-ta.dk>
2022-11-06 16:49:18 +0100

    hashrouter

--- src/root.js
+++ src/root.js
@@ -2,17 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Provider } from "react-redux";
 import App from "./App";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter /*BrowserRouter*/, Route, Routes } from 'react-router-dom';
 const Root = ({ store }) => {
 
     return (
         <Provider store={store}>
-            <BrowserRouter basename="/AlcaboneOrganisation">
+            <HashRouter basename="/">
+           {/*<BrowserRouter basename="">*/}
                 <Routes>
-                    <Route path="/AlcaboneOrganisation" element={<App />} />
+                    <Route path="/" element={<App />} />
                     {/*<Route path="/:wallet" element={<App />} />*/}
                 </Routes>
-            </BrowserRouter>
+           {/*</BrowserRouter>*/}
+           </HashRouter>
         </Provider >
     )
 };
